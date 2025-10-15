c2afa58849e47664aaf2f5579f6ae7a918319b12
samaro119 <samuel.lannigan19@gmail.com>
2025-10-15 20:01:33 +1100

    Update code.py

--- code.py
+++ code.py
@@ -50,7 +50,7 @@ layer_3 =     {6: ConsumerControlCode.VOLUME_DECREMENT, 10: ConsumerControlCode.
                4: ConsumerControlCode.SCAN_PREVIOUS_TRACK, 8: ConsumerControlCode.PLAY_PAUSE, 12: ConsumerControlCode.SCAN_NEXT_TRACK}
 
 # blue - Work
-layer_4 =     {4: "JmMlSt@0)0!2@0", 5: ["sam", Keycode.TAB, "Sam123", Keycode.TAB, "5536"],
+layer_4 =     {4: "Placeholder", 5: "wow",
                9: "Hope this helps!", 13: "Let me know if you need anything else!",
                6: "Thanks for your patience.", 10: "Thank you for your understanding.", 14: "Thanks for your help!",
                8: ["/catalog/product/view/sku/", (Keycode.LEFT_CONTROL, Keycode.V)],
@@ -273,3 +273,4 @@ while True:
     # If enough time has passed, reset the fired variable
     if fired and time.monotonic() - keybow.time_of_last_press > debounce:
         fired = False
+
